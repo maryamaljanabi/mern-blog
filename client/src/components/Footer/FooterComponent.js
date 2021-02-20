c5ecba5acdd546f666bc8cd966bbc9dd9b22c791
maryamaljanabi <maryam.aljanabi135@gmail.com>
2021-02-20 12:56:23 +0200

    Adjust the footer style

--- client/src/components/Footer/FooterComponent.js
+++ client/src/components/Footer/FooterComponent.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "./Footer.scss";
 
 export default function FooterComponent() {
-  return <div>footer</div>;
+  return (
+    <div className="footer-component">© MERN Blog 2021 by Maryam Aljanabi</div>
+  );
 }
