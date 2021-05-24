daf7a063542598812cb090cd9b7233c312fcec3b
maryamaljanabi <maryam.aljanabi135@gmail.com>
2021-05-24 15:31:55 +0300

    Add error and loader to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import "./Home.scss";
 import { Player } from "@lottiefiles/react-lottie-player";
-import { Button, Row, Col, Divider, Card } from "antd";
+import { Button, Row, Col, Divider, Card, Spin, Alert} from "antd";
 import { RightCircleOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
 import { postsAPI } from "../../api/api";
@@ -15,6 +15,7 @@ export default function Home() {
   const userState = useSelector((st) => st.user);
   const [postsData, setPostsData] = useState([]);
   const [width, setWidth] = useState(window.innerWidth);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     function handleResize() {
@@ -29,7 +30,9 @@ export default function Home() {
       try {
         const { data: res } = await postsAPI.getAll();
         setPostsData(res);
+        setErrorMsg(null);
       } catch (error) {
+        setErrorMsg('Error loading posts data')
         console.log("Error retrieving all posts...", error);
       }
     })();
@@ -129,6 +132,11 @@ export default function Home() {
         </>
       )}
       <Divider orientation="center">Most recent posts</Divider>
+      {errorMsg? 
+      <div className="loader-container">
+      <Alert message={errorMsg} type="error" /> 
+      </div>
+      : postsData && Boolean(postsData.length) ? 
       <Row className="posts-container" type="flex">
         {postsData.map((item) => (
           <Col xs={24} sm={12} md={8} lg={8} key={item._id}>
@@ -148,7 +156,11 @@ export default function Home() {
             </Card>
           </Col>
         ))}
-      </Row>
+      </Row> :
+      <div className="loader-container">
+        <Spin size="large" />     
+      </div>
+    }
     </div>
   );
 }
--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ function setupServer() {
   app.use("/api/posts", postRoutes);
   app.use("/api/users", userRoutes);
   app.use("/api/auth", authRoutes);
-  app.use(express.static(path.resolve(__dirname, "./../client/build")));
+  app.use(express.static(path.join(__dirname, "./../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./../client/build", "index.html"));
   });
